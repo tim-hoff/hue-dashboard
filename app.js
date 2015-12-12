46816381425ba83f65b619ae2de33fc54fac7f94
Tim Hoff <timothy.hoff@praeses.com>
2015-12-12 13:30:59 -0600

    form base

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var light = require('./routes/light');
-
 var app = express();
 
 // view engine setup
@@ -24,6 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes); // do middleware routes
 app.use('/light', light);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/light.js
+++ routes/light.js
@@ -3,9 +3,11 @@ var router = express.Router();
 var hue = require("node-hue-api");
 var HueApi = hue.HueApi;
 var lightState = hue.lightState;
-var hostname = "192.168.1.3";
+var hostname = "192.168.1.3"; // lab
+// var hostname = "192.168.1.109"; // home
 var username = "TimH1993zxcv";
 var userDescription = "The real M.V.P.";
+
 var api = new HueApi(hostname, username);
 var light_1 = 1, light_2 = 2, light_3 = 3;
 var lamp = 4, ls1 = 1, ls2 = 2;
@@ -16,7 +18,7 @@ function blinkLights(value) {
   var state_low = lightState.create().bri(1).rgb([100,0,255]);
   var state_med = lightState.create().bri(100).rgb([100,0,255]);
   var state_high = lightState.create().bri(254).rgb([100,0,255]);
-  var state_alert = lightState.create().bri(180).rgb([255,0,0]).alert("lselect");
+  var state_alert = lightState.create().bri(180).rgb([255,0,0]).alert("select");
 
   api.setLightState(value, state_alert).done();
 };
@@ -24,7 +26,7 @@ function blinkLights(value) {
 /* GET users listing. */
 
 router.use('/', function(req, res, next) {
-	[1,2,3,4].forEach(blinkLights);
+	[1,2,3].forEach(blinkLights);
   next();
 });
 
